f736f20158092f804ced6de3de6358ac454179ca
Duncan Ritchie <duncan.ritchie@informationcatalyst.com>
2020-03-28 22:02:12 +0000

    The photo visible changes according to the section in view

--- main.js
+++ main.js
@@ -12,30 +12,47 @@ for (let i = 0; i < navTickboxes.length; i++) {
     })
 }
 
-const main = document.getElementsByTagName("main")[0];
 const textWrapGuide = document.getElementById("text-wrap-guide");
 // Update scroll `target`, and start the animation if it is not running already.
 function moveTextWrapGuide () {
-    const windowHeight =  document.documentElement.clientHeight;
+    const windowHeight = document.documentElement.clientHeight;
     const newUpperEdge = window.scrollY || window.pageYOffset;
-    // const newUpperEdge = 0;
     const newLowerEdge = windowHeight + newUpperEdge;
     const docHeight = Math.max(document.documentElement.clientHeight, document.documentElement.offsetHeight, document.documentElement.scrollHeight);
     const newShapeOutside = "polygon(0 0, 100% 0, 100% "+(newUpperEdge/2)+"px, 90% "+newUpperEdge+"px, 90% "+newUpperEdge+"px, 100% "+newLowerEdge+"px, 100% "+docHeight+"px, 0 "+docHeight+"px)";
-    console.log(newUpperEdge);
-    console.log(newShapeOutside);
-    console.log(docHeight);
 
     textWrapGuide.style.height = docHeight + "px";
     textWrapGuide.style.shapeOutside = newShapeOutside;
-    main.height = Math.round((docHeight/30)+0.5);;
 }
 
-function updateScroll () {
-    window.setTimeout(moveTextWrapGuide, 100);
+const mainImages = document.getElementsByClassName("main-image");
+
+function showMainImages() {
+    const windowHeight = document.documentElement.clientHeight;
+    const newUpperEdge = window.scrollY || window.pageYOffset;
+    const indexOfSectionVisible = Math.round(newUpperEdge / windowHeight);
+    for (let i = 0; i < mainImages.length; i++) {
+        if (i == indexOfSectionVisible) {
+            //console.log("Showing image "+i);
+            mainImages[i].style.opacity = "1";
+        }
+        else {
+            //console.log("Hiding image "+i);
+            mainImages[i].style.opacity = "0";
+        }
+    }
+}
+
+function updateScroll() {
+    moveTextWrapGuide();
+    showMainImages();
+}
+
+function updateScrollWithTimeout() {
+    window.setTimeout(updateScroll, 100);
 }
   
 // Listen for `scroll` event to update the position of #text-wrap-guide.
-window.addEventListener("scroll", updateScroll)
+window.addEventListener("scroll", updateScrollWithTimeout);
 // Update scroll position on page load.
-window.addEventListener("load", moveTextWrapGuide)
+window.addEventListener("load", updateScroll);
