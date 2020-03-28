2bf4840350510b684c10db9b267ab08d6355decc
Duncan Ritchie <duncan.ritchie@informationcatalyst.com>
2020-03-28 17:33:42 +0000

    First commit of redesign

--- main.js
+++ main.js
@@ -1,54 +1,39 @@
-let  cujToggles = document.getElementsByClassName("cuj-toggle")
-let jumbotrons = document.getElementsByClassName("jumbotron-main")
-let showJumbotrons = false
-
-const addAnimatableAttribute = () => {
-    for (let jumbotron of jumbotrons) {
-        jumbotron.setAttribute("animatable","true")
-    }
-}
-
-const hideJumbotrons = () => {
-    for (let jumbotron of jumbotrons) {
-        jumbotron.style.opacity = 0
-        jumbotron.style.transform = "rotateX(90deg)"
-        showJumbotrons = false
-    }
-}
-
-const toggleJumbotrons = () => {
-    if (showJumbotrons) {
-        // This hides all jumbotrons on the page
-        for (j=0;j<jumbotrons.length;j++) {
-            jumbotrons[j].style.opacity = 0
-            jumbotrons[j].style.transform = "rotateX(90deg)"
+//// Allow no more than one nav tickbox to be ticked at a time.
+//// This results in no more than one nav submenu being open at a time.
+const navLabels = document.querySelectorAll("nav ul label");
+const navTickboxes = document.querySelectorAll("nav ul input");
+for (let i = 0; i < navTickboxes.length; i++) {
+    navLabels[i].addEventListener("click", (e)=>{
+        for (let j = 0; j < navTickboxes.length; j++) {
+            if (i !== j) {
+                navTickboxes[j].checked = false;
+            }
         }
-        // This changes the text to "Show text"
-        for (k=0;k<cujToggles.length;k++) {
-            cujToggles[k].textContent = "Show text"
-        }
-        showJumbotrons = false
-    }
-    else {
-        // This shows all jumbotrons on the page
-        for (j=0;j<jumbotrons.length;j++) {
-            jumbotrons[j].style.opacity = 1
-            jumbotrons[j].style.transform = "rotateX(0)"
-        }
-        // This changes the text to "Hide text"
-        for (k=0;k<cujToggles.length;k++) {
-            cujToggles[k].textContent = "Hide text"
-        }
-        showJumbotrons = true
-    }
-}
-
-for (i=0;i<cujToggles.length;i++) {
-    cujToggles[i].addEventListener("click",()=>{
-        toggleJumbotrons()
     })
 }
 
-hideJumbotrons()
-window.setTimeout(addAnimatableAttribute,0)
-window.setTimeout(toggleJumbotrons,0)
+const main = document.getElementsByTagName("main")[0];
+const textWrapGuide = document.getElementsByClassName("text-wrap-guide")[0];
+// Update scroll `target`, and start the animation if it is not running already
+function moveTextWrapGuide () {
+    const windowHeight =  document.documentElement.clientHeight;
+    const newUpperEdge = window.scrollY || window.pageYOffset;
+    // const newUpperEdge = 0;
+    const newLowerEdge = windowHeight + newUpperEdge;
+    const docHeight = Math.max(document.documentElement.clientHeight, document.documentElement.offsetHeight, document.documentElement.scrollHeight);
+    const newShapeOutside = "polygon(0 0, 90% 0, 90% "+newUpperEdge+"px, 100% "+newLowerEdge+"px, 100% "+docHeight+"px, 0 "+docHeight+"px)";
+    console.log(newUpperEdge);
+    console.log(newShapeOutside);
+    console.log(docHeight);
+
+    textWrapGuide.style.height = docHeight + "px";
+    textWrapGuide.style.shapeOutside = newShapeOutside;
+    main.height = Math.round((docHeight/30)+0.5);;
+}
+
+function updateScroll () {
+    window.setTimeout(moveTextWrapGuide, 100);
+}
+  
+// Listen for `scroll` event to update `target` scroll position
+window.addEventListener("scroll", updateScroll)
