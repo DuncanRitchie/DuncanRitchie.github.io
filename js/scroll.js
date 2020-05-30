7c843655c746f0bf8f57c7cf583433182bfb9f2b
Duncan Ritchie <duncan.ritchie@informationcatalyst.com>
2020-05-30 21:52:17 +0100

    Improved appearance on mobile

--- js/scroll.js
+++ js/scroll.js
@@ -1,39 +1,47 @@
 //// The code to change the main images uses Intersection Observer.
 //// The code to change text-wrap-guide does not.
 
+const isViewportBigEnoughForScrollBehaviour = () => {
+    return document.documentElement.clientWidth > 600;
+}
+
 const mainImages = document.getElementsByClassName("main-image");
 
 if (!!window.IntersectionObserver) {
     //// Find which mainImage should be displayed, then change styling on mainImages accordingly.
-    const sections =  document.querySelectorAll("section:not(.showcase-group)");
+    const sections =  document.getElementsByTagName("section");
     const scrollCallback = (entries, observer) => {
-        //// Find which mainImage should be displayed by finding which section is intersecting with screen.
-        //// If two sections are on the screen, indexOfSectionVisible will be set to the first section
-        //// in one loop iteration and then set to the second section in another loop iteration, so 
-        //// it will be the second section that will determine which mainImage is displayed.
-        let indexOfSectionVisible = -1;
-        for (let i = 0; i < entries.length; i++) {
-            for (let j = 0; j < sections.length; j++) {
-                if (entries[i].target.id === sections[j].id
-                    && entries[i].intersectionRatio > 0.1) {
-                    indexOfSectionVisible = j + 1;
+        if (isViewportBigEnoughForScrollBehaviour()) {
+            //// Find which mainImage should be displayed by finding which section is intersecting with screen.
+            //// If two sections are on the screen, indexOfSectionVisible will be set to the first section
+            //// in one loop iteration and then set to the second section in another loop iteration, so 
+            //// it will be the second section that will determine which mainImage is displayed.
+            let indexOfSectionVisible = -1;
+            for (let i = 0; i < entries.length; i++) {
+                for (let j = 0; j < sections.length; j++) {
+                    if (entries[i].target.id === sections[j].id
+                        && entries[i].intersectionRatio > 0.1) {
+                        indexOfSectionVisible = j + 1;
+                        
+                        console.log(mainImages[indexOfSectionVisible])
+                    }
                 }
             }
-        }
-        //// indexOfSectionVisible will still be -1 if the observer has fired, but not reported an intersection.
-        if (indexOfSectionVisible > -1) {
-            for (let i = 0; i <= sections.length; i++) {
-                if (i < indexOfSectionVisible) {
-                    mainImages[i].className = "main-image hidden";
-                    mainImages[i].style.zIndex = 1;
-                }
-                else if (i == indexOfSectionVisible) {
-                    mainImages[i].className = "main-image";
-                    mainImages[i].style.zIndex = 0;
-                }
-                else {
-                    mainImages[i].className = "main-image hidden";
-                    mainImages[i].style.zIndex = 0;
+            //// indexOfSectionVisible will still be -1 if the observer has fired, but not reported an intersection.
+            if (indexOfSectionVisible > -1) {
+                for (let i = 0; i <= sections.length; i++) {
+                    if (i < indexOfSectionVisible) {
+                        mainImages[i].className = "main-image hidden";
+                        mainImages[i].style.zIndex = 1;
+                    }
+                    else if (i == indexOfSectionVisible) {
+                        mainImages[i].className = "main-image";
+                        mainImages[i].style.zIndex = 0;
+                    }
+                    else {
+                        mainImages[i].className = "main-image hidden";
+                        mainImages[i].style.zIndex = 0;
+                    }
                 }
             }
         }
@@ -46,14 +54,6 @@ if (!!window.IntersectionObserver) {
         intersectionObserver.observe(sections[i]);
     }
 }
-
-// const pageHasProjectShowcase = !!document.getElementsByClassName("showcase-group")[0];
-
-// if (pageHasProjectShowcase) {
-//     console.log("Top offset of showcase: ", document.getElementsByClassName("showcase-group")[0].offsetTop);
-//     console.log("Top offset of velut: ", document.getElementById("velut").offsetTop);
-// }
-
 
 //// Code to move text-wrap-guide. It uses scroll position, but not Intersection Observer.
 
@@ -69,11 +69,13 @@ if (!!textWrapGuide) {
     }
 
     function updateScroll() {
-        const windowHeight = document.documentElement.clientHeight;
-        const distanceScrolled = window.scrollY || window.pageYOffset;
-        const startOfTextWrapping = 0;
-        const endOfTextWrapping = Math.max(document.documentElement.clientHeight, document.documentElement.offsetHeight, document.documentElement.scrollHeight);
-        moveTextWrapGuide(windowHeight, distanceScrolled, startOfTextWrapping, endOfTextWrapping);
+        if (isViewportBigEnoughForScrollBehaviour()) {            
+            const windowHeight = document.documentElement.clientHeight;
+            const distanceScrolled = window.scrollY || window.pageYOffset;
+            const startOfTextWrapping = 0;
+            const endOfTextWrapping = Math.max(document.documentElement.clientHeight, document.documentElement.offsetHeight, document.documentElement.scrollHeight);
+            moveTextWrapGuide(windowHeight, distanceScrolled, startOfTextWrapping, endOfTextWrapping);
+        }
     }
 
     function updateScrollWithTimeout() {
