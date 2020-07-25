13959027e0ef72abfe8d23582686c1614c85236e
Duncan Ritchie <duncan.ritchie@informationcatalyst.com>
2020-07-25 16:43:04 +0100

    Submenus on navbar can be opened by keyboard

--- js/navbar.js
+++ js/navbar.js
@@ -19,6 +19,49 @@ for (let i = 0; i < navSubmenuItems.length; i++) {
         for (let j = 0; j < navTickboxes.length; j++) {
             navTickboxes[j].checked = false;
         }
-    })
+    });
 }
 
+//// When a nav label is focused, keypresses should have the effect of clicks (of opening/hiding the submenu)
+for (let i = 0; i < navLabels.length; i++) {
+    navLabels[i].addEventListener("keydown", (e)=>{
+        //// On Arrow Down.
+        if (e.keyCode == 40) {
+            let firstItemOfSubmenu = document.querySelectorAll("nav input:checked ~ label:focus ~ ul li a")[0];
+            //// If the current submenu is not open, it needs to be opened for its first item to be found.
+            if (!firstItemOfSubmenu) {
+                navLabels[i].click();
+                firstItemOfSubmenu = document.querySelectorAll("nav input:checked ~ label:focus ~ ul li a")[0];
+            }
+            firstItemOfSubmenu.focus();
+            return;
+        }
+        //// On Arrow Up.
+        if (e.keyCode == 38) {
+            //// If the current submenu is open, close it.
+            document.querySelectorAll("nav input:checked ~ label:focus") && navLabels[i].click();
+            return;
+        }
+        //// If the key pressed is not Tab or Shift.
+        if (e.keyCode !== 9 && e.keyCode !== 16) {
+            navLabels[i].click();
+        }
+    });
+}
+
+for (let i = 0; i < navSubmenuItems.length; i++) {
+    navSubmenuItems[i].addEventListener("keydown", (e)=>{
+        switch (e.keyCode) {
+            case 38:
+                //// Go up an item.
+                i==0 || navSubmenuItems[i-1].focus();
+                break;
+            case 40:
+                //// Go down an item.
+                i==navSubmenuItems.length || navSubmenuItems[i+1].focus();
+                break;
+            default:
+                break;
+        }
+    });
+}
